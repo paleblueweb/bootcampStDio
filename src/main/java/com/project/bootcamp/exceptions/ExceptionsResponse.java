582d74fb63251d8531d3270d95d9c740b5b0d278
paleblueweb <paleblue.web@outlook.com>
2021-05-28 14:20:35 -0300

    uploading backend

---
+++ src/main/java/com/project/bootcamp/exceptions/ExceptionsResponse.java
@@ -0,0 +1,13 @@
+package com.project.bootcamp.exceptions;
+
+public class ExceptionsResponse {
+    private String message;
+
+    public ExceptionsResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
